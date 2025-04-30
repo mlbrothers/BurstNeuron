54ad8e57c2892cdb89bc7469aa5713c81205d525
Siddharth <siddharth2000patel@gmail.com>
2025-04-30 18:49:02 +0530

    plant identifier

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def home():
 def tip_calculator():
     return render_template('tip_calculator.html')
 
+@app.route('/plant-identifier')
+def plant_identifier():
+    return render_template('plant_identifier.html')
+
 @app.route('/app-ads.txt')
 def serve_ads_txt():
     return send_from_directory('static', 'app-ads.txt')
